7026def968dd57e843158f4927c54a3506dcc43c
Elliott D'Alvarez <edalvarez@pixsystem.com>
2022-12-07 14:42:48 +0000

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Elliott D'Alvarez on 07/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
